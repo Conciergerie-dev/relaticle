93c3aa351d9459ab19cd9fcc14f3a267fa6de3ba
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2026-02-05 15:41:43 +0100

    fix: enable trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,6 +38,9 @@ return Application::configure(basePath: dirname(__DIR__))
             return route('login');
         });
     })
+    ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         Integration::handles($exceptions);
     })
